204ab09e65dddef7164220f15974807115184f46
Brandon Flores <brandonjaylapaflores@gmail.com>
2017-11-08 00:00:05 +0800

    employee class

--- src/main/java/com/database/Employee.java
+++ src/main/java/com/database/Employee.java
@@ -1,23 +1,55 @@
 package com.database;
 
 public class Employee{
-	private String name, id, position;
+	private String username, password;
+	private String firstName, lastName;
+	private String workType;
 
-	public Employee(String name, String id, String position){
-		this.name = name;
-		this.id = id;
-		this.position = position;
+	public Employee(String username, String password, String firstName, String lastName, String workType) {
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.workType = workType;
 	}
 
-	public String getName(){
-		return name;
+	public String getUsername() {
+		return username;
 	}
 
-	public String getID(){
-		return id;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public String getPosition(){
-		return position;
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getWorkType() {
+		return workType;
+	}
+
+	public void setWorkType(String workType) {
+		this.workType = workType;
 	}
 }
