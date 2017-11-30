45e8eb9c072ff54f17e9d646304e62a3da0c4bc6
Carlo <miguelcarlofernandez@gmail.com>
2017-11-30 22:26:30 +0800

    Checks if lower and higher already exists

--- src/main/java/com/cashier/ui/FXMLGetRangeController.java
+++ src/main/java/com/cashier/ui/FXMLGetRangeController.java
@@ -1,8 +1,8 @@
 package com.cashier.ui;
 
+import com.database.Fee;
 import com.database.RangeOR;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,7 +13,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class FXMLGetRangeController implements Initializable {
     Stage anotherStage = new Stage();
@@ -31,6 +35,9 @@ public class FXMLGetRangeController implements Initializable {
     private JFXButton cancel;
 
     private DatabaseReference database;
+    private ArrayList<Fee> fees;
+    private Lock lock = new ReentrantLock();
+    private Condition cond = lock.newCondition();
 
     @FXML
     void cancelPressed(ActionEvent event) throws IOException {
@@ -39,11 +46,30 @@ public class FXMLGetRangeController implements Initializable {
         stage.close();
     }
 
+    private boolean lowFeeExists() {
+        for(Fee f : fees) {
+            int orInt = Integer.parseInt(f.getOrNum());
+            if (f.getOrNum().equals(lowRange.getText())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean highFeeExists() {
+        for (Fee f : fees) {
+            int orInt = Integer.parseInt(f.getOrNum());
+            if (f.getOrNum().equals(highRange.getText())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @FXML
     void enterPressed(ActionEvent event) throws IOException {
         String low = lowRange.getText();
         String high = highRange.getText();
-
         low = low.replaceAll("\\s","");//removes spaces
         high = high.replaceAll("\\s","");
         if(low.equals("") || high.equals("")) {
@@ -51,19 +77,65 @@ public class FXMLGetRangeController implements Initializable {
         }
         else{
             //String original = "050";
-            int valueLow  = Integer.parseInt( low, 10);
+            int valueLow  = Integer.parseInt(low, 10);
             int valueHigh = Integer.parseInt(high, 10);
             if(valueLow >= valueHigh){
                 lblError.setText("* - Invalid Range");
             }else {
-                DatabaseReference ref = database.child("Range");
-                RangeOR range = new RangeOR(valueLow,valueHigh);
-                ref.setValue(range);
-                //FXMLMainCashierWindowController.setRange(valueLow, valueHigh, true);
-                //close void window then open void request window
-                Stage stage = (Stage) cancel.getScene().getWindow();
-                CashierMain.cancelPressed = true;
-                stage.close();
+                DatabaseReference gatherFeesRef = database.child("Fees");
+                gatherFeesRef.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+                        lock.lock();
+
+                        Fee fee = snapshot.getValue(Fee.class);
+                        fees.add(fee);
+
+                        cond.signal();
+                        lock.unlock();
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot snapshot, String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot snapshot, String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+
+                    }
+                });
+                lock.lock();
+                try { //try block for lock catching
+                    cond.await();
+                    if(lowFeeExists()) {
+                        lblError.setText("* - Low Range OR already exists");
+                    } else if(highFeeExists()) {
+                        lblError.setText("* - High Range OR already exists");
+                    } else {
+                        DatabaseReference ref = database.child("Range");
+                        RangeOR range = new RangeOR(valueLow, valueHigh);
+                        ref.setValue(range);
+                        //FXMLMainCashierWindowController.setRange(valueLow, valueHigh, true);
+                        //close void window then open void request window
+                        Stage stage = (Stage) cancel.getScene().getWindow();
+                        CashierMain.cancelPressed = true;
+                        stage.close();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                lock.unlock();
             }
 
         }
@@ -71,6 +143,7 @@ public class FXMLGetRangeController implements Initializable {
 
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
+        fees = new ArrayList<>();
         database = FirebaseDatabase.getInstance().getReference();
         lblError.setText("");
     }
